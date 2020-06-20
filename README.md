a45857dc4a84f4d36b1b1e338c9e451e9db6062e
Kevin Ochanda <kevohyout44@gmail.com>
2020-06-20 15:02:56 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+# bong
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+=======
+# Bong
+A Vue.js app
+>>>>>>> 6199d4bcab861f202abf447fead2f1aed160950c
---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="main-app" class="container">
+    <div class="row justify-content-center">
+      <appointment-list :appointments="appointments" @remove="removeItem" @edit="editItem" />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppointmentList from "./components/AppointmentList";
+import axios from "axios";
+import _ from "lodash";
+
+export default {
+  name: "MainApp",
+  data: function() {
+    return {
+      appointments: [],
+      aptIndex: 0
+    };
+  },
+  components: {
+    AppointmentList
+  },
+  mounted() {
+    axios.get("./data/appointments.json").then(
+      response =>
+        (this.appointments = response.data.map(item => {
+          item.aptId = this.aptIndex;
+          this.aptIndex++;
+          return item;
+        }))
+    );
+  },
+  methods: {
+    removeItem: function(apt) {
+      this.appointments = _.without(this.appointments, apt);
+    },
+    editItem: function(id, field, text) {
+      const aptIndex = _.findIndex(this.appointments, {
+        aptId: id
+      });
+      this.appointments[aptIndex][field] = text;
+    }
+  }
+};
+</script>
